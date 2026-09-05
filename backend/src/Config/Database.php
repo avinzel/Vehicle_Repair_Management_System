6c92d9aaff573cfde431bbbe66ed773e9aa5d66d
Noel <noelmercadal2124@gmail.com>
2026-09-06 03:52:10 +0800

    initialize shadcn/ui with Base UI and Nova preset

--- backend/src/Config/Database.php
+++ backend/src/Config/Database.php
@@ -5,7 +5,7 @@
     class Database{
         private $host = "localhost"; 
         private $user = "root";
-        private $pass = ""; 
+        private $pass = "mercadal123"; 
         private $db = "VehicleRepair";
 
         private $conn; 
