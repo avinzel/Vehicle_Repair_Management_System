35b77aa35ea74381efd57eb1fd417c9689d8af75
Vincent <mandapvincent291@gmail.com>
2026-09-08 21:32:58 +0800

    fixed naming

--- vehicle-repair-management-system/src/App.jsx
+++ vehicle-repair-management-system/src/App.jsx
@@ -8,7 +8,7 @@ import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 import {MechanicPage} from "./pages/MechanicPage/MechanicPage"
 import {Loading} from "./components/Loading"
 import { AdminPage } from './pages/AdminPage/AdminPage';
-import { DashBoardTab } from './pages/ServiceAdvisorPage/DashBoardTab';
+import { DashBoardTab } from './pages/ServiceAdvisorPage/DashboardTab';
 import Intake from './pages/ServiceAdvisorPage/Intake';
 import { ActiveRepairOrder } from './pages/ServiceAdvisorPage/ActiveRepairOrder';
 import { CustomerRecords } from './pages/ServiceAdvisorPage/CustomerRecords';
